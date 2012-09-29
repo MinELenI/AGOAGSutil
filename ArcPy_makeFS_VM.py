0c5a1a57764c6a0d166a411a0766117dcce995bb
maartentromp <maartentrompie@gmail.com>
2012-09-29 08:53:07 +0200

    import AgoUtil vervangen door AgsUtil

--- ArcPy_makeFS_VM.py
+++ ArcPy_makeFS_VM.py
@@ -1,29 +1,29 @@
 #-------------------------------------------------------------------------------
-# Name:        maak een service van aangemaakte dataset
-# Purpose:     maak een service van een automatisch aangemaakte dataset
-#              versie voor VM_ware
-# Author:      wubbels
+# Name:		maak een service van aangemaakte dataset
+# Purpose:	 maak een service van een automatisch aangemaakte dataset
+#			  versie voor VM_ware
+# Author:	  wubbels
 #
-# Created:     30-08-2012
+# Created:	 30-08-2012
 # Copyright:   (c) wubbels 2012
 #-------------------------------------------------------------------------------
 
 
-import AgoUtil
+import AgsUtil
 
 def main():
-    # define local variables
-    mapDoc = r"D:\Data\test.mxd"
-    serviceNaam = "test"
-    summary = 'testje'
-    tags = "test, gcc"
-    id_group = "**"
-    username = "***"
-    password = "**"
+	# define local variables
+	mapDoc = r"D:\Data\test.mxd"
+	serviceNaam = "test"
+	summary = 'testje'
+	tags = "test, gcc"
+	id_group = "**"
+	username = "***"
+	password = "**"
 	server = "www.arcgis.com"
 	
-    AgoUtil.publishMxdToAgo(mapDoc,serviceNaam,summary,tags,id_group,username,password,server)
+	AgsUtil.publishMxdToAgo(mapDoc,serviceNaam,summary,tags,id_group,username,password,server)
 
 #run main def
 if __name__ == '__main__':
-    main()
+	main()
